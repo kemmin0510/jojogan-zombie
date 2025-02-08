317ec16c63780551a4dd8f64669ec5a9c5d6cf87
khoaminh100597 <khoaminh.energy@gmail.com>
2025-02-08 17:48:14 +0700

    feat: change build_deploy_local.sh

--- bin/build_deploy_local.sh
+++ bin/build_deploy_local.sh
@@ -3,6 +3,11 @@
 # Assign port in localhost to 8000 port of container
 PORT=$1
 
+if docker ps -a | awk '{print $1}' | grep -w test; then
+    docker stop test
+    docker rm test
+fi
+
 if docker images | awk '{print $1}' | grep -w test; then
     docker rmi -f test
 fi
