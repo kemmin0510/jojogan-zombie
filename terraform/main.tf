41525e2625d739ee100ee3b1c89df18bfdcb2c54
khoaminh100597 <khoaminh.energy@gmail.com>
2025-02-11 20:13:16 +0700

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+# To define that we will use GCP
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.80.0" // Provider version
+    }
+  }
+  required_version = "1.10.5" // Terraform version
+}
+
+// The library with methods for creating and
+// managing the infrastructure in GCP, this will
+// apply to all the resources in the project
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
+// Google Kubernetes Engine
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+
+  // Enabling Autopilot for this cluster
+  enable_autopilot = true
+}
