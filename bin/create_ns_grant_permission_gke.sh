105b3c46809d590822bfee16c3a8683538a4b7c2
khoaminh100597 <khoaminh.energy@gmail.com>
2025-02-13 23:42:51 +0700

    grant gke permission to jenkins

--- bin/create_ns_grant_permission_gke.sh
+++ bin/create_ns_grant_permission_gke.sh
@@ -16,3 +16,23 @@ kubectl create clusterrolebinding anonymous-admin-binding \
   --clusterrole=admin \
   --user=system:anonymous \
   --namespace=model-serving
+
+kubectl create role jenkins-admin \
+  --verb=get,list,watch,create,update,patch,delete \
+  --resource=secrets,configmaps,services,deployments,daemonsets,statefulsets,pods \
+  --namespace=model-serving
+
+kubectl create role jenkins-admin \
+  --verb=get,list,watch,create,update,patch,delete \
+  --resource=secrets,configmaps,services,deployments,daemonsets,statefulsets,pods \
+  --namespace=kube-metrics
+
+kubectl create role jenkins-admin \
+  --verb=get,list,watch,create,update,patch,delete \
+  --resource=secrets,configmaps,services,deployments,daemonsets,statefulsets,pods \
+  --namespace=kube-metrics
+
+kubectl create rolebinding jenkins-admin-binding \
+  --role=jenkins-admin \
+  --serviceaccount=kube-metrics:default \
+  --namespace=kube-metrics
