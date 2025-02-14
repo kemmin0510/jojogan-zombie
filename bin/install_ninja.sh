8bed1c7ecfa4c9bbd4293ea801455a29ccbe081b
khoaminh100597 <khoaminh.energy@gmail.com>
2025-02-14 18:49:20 +0700

    keep only readme

--- bin/install_ninja.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
-sudo unzip ninja-linux.zip -d /usr/local/bin/
-sudo rm ninja-linux.zip
-sudo update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force
